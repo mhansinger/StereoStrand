ac153607d0092fde00d52d34e41442fd9793bfa0
mhansinger <maximilian.hansinger@gmail.com>
2019-07-21 18:45:42 +0200

    runscript

--- stereostrand_main.py
+++ stereostrand_main.py
@@ -2,11 +2,11 @@ import time
 import numpy as np
 from midi_connector import midi_connector
 
-WINDOW = 1000
+WINDOW = 100
 
 CHANNELS = 9
 
-INTERVAL = input('Zeitintervall zwischen den Sounds, z.B. 2 heißt alle 2 sek. einen Ton\n')
+INTERVAL = input('\n\nZeitintervall zwischen den Sounds, z.B. 2 heißt alle 2 sek. einen Ton\n')
 
 try:
     INTERVAL = float(INTERVAL)
@@ -16,9 +16,12 @@ except ValueError:
 midi = midi_connector(WINDOW=WINDOW,CHANNELS=CHANNELS)
 
 def play():
+    print('\nIt`s on!')
+    print('Press Ctrl+c to abort')
     while True:
         midi.send_signal()
-        time.sleep(INTERVAL)
+
+        time.sleep(INTERVAL*np.random.rand()+INTERVAL)
 
 if __name__=='__main__':
     play()
