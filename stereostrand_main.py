08dd22324a72657e6a4bb23670835b48ef2dec4d
mhansinger <maximilian.hansinger@gmail.com>
2019-07-21 19:57:34 +0200

    beats interval

--- stereostrand_main.py
+++ stereostrand_main.py
@@ -1,33 +1,60 @@
 import time
 import numpy as np
 from midi_connector import midi_connector
-from myASCII import ascii_greeter
+
+from asciimatics.effects import Cycle, Stars
+from asciimatics.renderers import FigletText
+from asciimatics.scene import Scene
 from asciimatics.screen import Screen
+
+# Startbildschirm
+def ascii_greeter(screen):
+    effects = [
+        Cycle(
+            screen,
+            FigletText("StereoStrand", font='big'),
+            int(screen.height / 2 - 8)),
+        Cycle(
+            screen,
+            FigletText("ROCKS!", font='big'),
+            int(screen.height / 2 + 3)),
+        Stars(screen, 200)
+    ]
+    screen.play([Scene(effects, 100)],repeat=False)
 
 
 WINDOW = 100
 
 CHANNELS = 9
 
-INTERVAL = input('\n\nZeitintervall zwischen den Sounds, z.B. 2 heißt alle 2 sek. einen Ton\n')
+INTERVAL = int(input('\n\nZeitintervall zwischen den Sounds, z.B. 2 heißt alle 2 sek. einen Ton\n'))
 
-BEATS_INTERVAL = input('\n\nWieviele Minuten zwischen den Beats?\n')
+BEATS_INTERVAL = int(input('\n\nWieviele Minuten zwischen den Beats?\n'))
 
 # BEATS_INTERVAL in sec
 BEATS_INTERVAL=BEATS_INTERVAL*60
 
-LIEBLINGS_ZAHL = input('\n\nWas ist deine Lieblingszahl (1-1000)?\n')
+LIEBLINGS_ZAHL = int(input('\n\nWas ist deine Lieblingszahl (1-1000)?\n'))
 
 COUNTER = 0
-
-
-try:
-    INTERVAL = float(INTERVAL)
-except ValueError:
-    print('Muss eine Zahl sein!')
+#
+#
+# try:
+#     INTERVAL = float(INTERVAL)
+# except ValueError:
+#     print('Muss eine Zahl sein!')
 
 # ASCII bild
-Screen.wrapper(ascii_greeter)
+try:
+    Screen.wrapper(ascii_greeter)
+    # klappt nie beim ersten mal???
+except:
+    pass
+
+try:
+    Screen.wrapper(ascii_greeter)
+except:
+    pass
 
 midi = midi_connector(WINDOW=WINDOW,CHANNELS=CHANNELS)
 
@@ -58,7 +85,9 @@ def play():
             diff_time = jetzt_zeit - time_start
 
             if diff_time > BEATS_INTERVAL:
+                print('\nTHE BEAT IS ON!\n')
                 midi.test_send(9)       # schick signal an Kanal 10 (ist in python die nr 9)
+                time_start = jetzt_zeit
 
     except KeyboardInterrupt:
         midi.quit_midi()
