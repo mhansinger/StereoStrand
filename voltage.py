39443a1ac125d8e7d3c73486bdb26068025f8bd4
Maximilian Hansinger <maximilian.hansinger@gmail.com>
2020-03-12 11:54:33 +0100

    Update voltage.py

--- voltage.py
+++ voltage.py
@@ -61,25 +61,3 @@ class voltage(object):
 
 
 
-
-
-
-
-# SHUNT_OHMS = 0.1
-#
-# def read():
-#     ina = INA219(SHUNT_OHMS)
-#     ina.configure()
-#
-#     print("Bus Voltage: %.3f V" % ina.voltage())
-#     try:
-#
-#         print("Shunt voltage: %.3f mV" % ina.shunt_voltage())
-#     except DeviceRangeError as e:
-#         # Current out of device range with specified shunt resister
-#         print(e)
-#
-#
-# for i in range(1000):
-#     read()
-#     time.sleep(1) #pause 1 sec
